14b83de6c1ca9d39cb77eb83936734b06b6647ac
aryadavare19 <153051485+aryadavare19@users.noreply.github.com>
2025-04-16 15:32:08 +0530

    added safe route option

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from report import report_window
 from view_reports import view_reports_window
+from safe_route_ui import open_safe_route_window
 
 def open_main_menu(user):
     menu = tk.Toplevel()
@@ -11,4 +12,6 @@ def open_main_menu(user):
     tk.Label(menu, text=f"Welcome, {user[1]}").pack(pady=10)
     tk.Button(menu, text="Add Report", command=lambda: report_window(user)).pack(pady=5)
     tk.Button(menu, text="View My Reports", command=lambda: view_reports_window(user)).pack(pady=5)
+    tk.Button(menu, text="Get Safe Route", command=open_safe_route_window).pack(pady=10)
+
     tk.Button(menu, text="Logout", command=menu.destroy).pack(pady=5)
